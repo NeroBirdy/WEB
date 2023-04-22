1690f893e127c789b7abbfef00f19254300cff99
BirdyNero <nirvanav.39@gmail.com>
2023-04-22 23:03:29 +0500

    Добавил Очередь(Вроде преоритетную) и Astar

--- Astar.js
+++ Astar.js
@@ -135,7 +135,134 @@ function Click(event)
 
 }
 
+function Queue()
+{
+    let Set = [];
 
+    this.add_to = function(cell)//Добавляем ячейку и расстояние 
+    {
+        if (this.Is_Empty)
+            {
+                Set.push(cell);
+            }
+        else
+        {
+            for (let i = 0; i < Set.length; i++)
+            {
+                if (cell[1] < Set[i][1])
+                {
+                    Set.splice(i, 0, cell);
+                }
+            }
+        }
+    }
+    
+    this.take_first = function()
+    {
+        return Set.shift();
+    }
+
+    this.Is_Empty = function()
+    {
+        return Set.length === 0;
+    }
+}
+
+function heuristic(cur, end)
+{
+    return Math.abs(cur[0] - end[0]) + Math.abs(cur[1] - end[1]);
+}
+
+function getNeigbors(cur, end, G)
+{
+    let neighbours = [];
+    let x = cur[0][0];
+    let y = cur[0][1];
+    if(y != 0 && !matrix[x][y - 1] && G[x][y - 1])
+    {
+        neighbours.push([x, y - 1]);
+    }
+    if(y != n - 1 && !matrix[x][y + 1]&& G[x][y + 1])
+    {
+        neighbours.push([x, y + 1]);
+    }
+    if(x != 0 && !matrix[x - 1][y]&& G[x - 1][y])
+    {
+        neighbours.push([x - 1, y]);
+    }
+    if(x != n - 1 & !matrix[x + 1][y]&& G[x + 1][y])
+    {
+        neighbours.push([x + 1, y]);
+    }
+
+    if(!x && !y && !matrix[x - 1][y - 1]&& G[x - 1][y - 1])
+    {
+        neighbours.push([x - 1, y - 1]);
+    }
+    if(!x && y != n - 1 && !matrix[x - 1][y + 1]&& G[x - 1][y + 1])
+    {
+        neighbours.push([x - 1, y + 1]);
+    }
+    if(x != n - 1 && !y && !matrix[x + 1][y - 1]&& G[x + 1][y - 1])
+    {
+        neighbours.push([x + 1, y - 1]);
+    }
+    if(x != n - 1 && y != n - 1 && !matrix[i + 1][y + 1]&& G[x + 1][y + 1])
+    {
+        neighbours.push([x + 1, y + 1]);
+    }
+    return neighbours;
+}
+
+function Astar(start, end)
+{
+    let queue = new Queue();
+    let GScores = getMatrix(n, -1);
+
+    GScores[start[0]][start[1]] = 0;
+    let parents = new Array(n);//Инициализируем Массив Родителей, позже понадобится для окраски пути
+    for (let i = 0; i < n; i++)
+    {
+        parents[i] = new Array(n)
+        for (let j = 0; j < n; j++)
+        {
+            parents[i][j] = new Array(2);
+            parents[i][j][0] = -1;
+            parents[i][j][1] = -1;
+        }
+    }
+    queue.add_to([start, heuristic(start, end)]);
+    while(!queue.Is_Empty)
+    {
+        let current = queue.take_first();
+        if (current[0][0] === end[0] && current[0][1] === end[1])
+        {
+            break;
+        }
+
+        let neighbours = getNeigbors(current, matrix, GScores);
+        for (let i = 0; i < neighbours.length; i++)
+        {
+            let neigbor = neighbours[i];
+
+            context.fillStyle = "#cdcdcd";
+            context.fillRect(neigbor[1] * cube, neigbor[0] * cube, cube, cube);
+
+            let nX = neigbor[0];
+            let nY = neigbor[1];
+            let cX = current[0][0];
+            let cY = current[0][1];
+
+            if (GScores[nX, nY] === -1 || GScores[cX][cY] + 1 > GScores[nX][nY])
+            {
+                parents[nX][nY][0] = cX;
+                parents[nX][nY][1] = cY;
+                GScores[nX][nY] = Gscores[cX][cY] + 1;
+                queue.add_to([neigbor, Gscores[nX][nY]] + heuristic(neigbor, end));
+            }
+        }
+    }
+}
 
 
 
