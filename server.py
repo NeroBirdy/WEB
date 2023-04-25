edf9982d3272ea01b3f342285cc146a02fb7208d
lollipop4253 <mishamx04@gmail.com>
2023-04-25 12:32:03 +0500

    create server.py and edit other files

---
+++ server.py
@@ -0,0 +1,22 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route("/")
+def Home_page():
+	return "Home page"
+
+@app.route("/astar")
+def Astar_page():
+	return flask.render_template("Astar.html")
+
+@app.route("/claster")
+def Claster_page():
+	return flask.render_template("KL.html")
+
+@app.route("/genetic")
+def Gen_algorithm():
+	return flask.render_template("genetic.html")
+
+if __name__ == "__main__":
+	app.run(debug=True, host="0.0.0.0")
